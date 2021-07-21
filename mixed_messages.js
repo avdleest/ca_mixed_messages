423d387e4606d2123225acffbc68cac6e4f1aeb4
Arthur van der Leest <avdleest@gmail.com>
2021-07-21 18:34:58 +0200

    Changed generation of random item

--- mixed_messages.js
+++ mixed_messages.js
@@ -3,21 +3,21 @@ Mixed messages produces a message with random content from three arrays.
 The message has the following structure:
 `Did you know that [A person or animal] has [some achievement or possession] [timespan]?`*/
 
-let subject = ['Trump', 'your mother', 'my guinea pig', 'your grandpa', 'your car', 'the Pope', 'his sister']
-let posessionOrAchievement =  ['has the largest balls', 'has petted the most guinea pigs', 'has held the best speech', 'drifted the longest distance', 'won the most races']
+let subject = ['Trump', 'your mother', 'my guinea pig', 'your grandpa', 'your car', 'the Pope', 'his sister', 'the rabbit']
+let posessionOrAchievement =  ['has petted the most guinea pigs', 'has held the best speech', 'drifted the longest distance', 'won the most races']
 let timespan = ['in human history', 'of all times', 'today', 'presently']
 
 let components = [subject, posessionOrAchievement, timespan]
 
-const generateArrayNumber = (arr) => {
-    return Math.floor(Math.random() * arr.length);
+const getRandomItem = (arr) => {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index]
 }
 
 const generateMixedMessage = (arr) => {
-    const indexArray = arr.map(el => generateArrayNumber(el))
-    let [sI, pI, tI]  = indexArray
+    let [subject, posession, timespan] = arr.map(el => getRandomItem(el))
 
-    console.log(`${subject[sI]} ${posessionOrAchievement[pI]} ${timespan[tI]}`);
+    console.log(`${subject} ${posession} ${timespan}`);
 }
 
 generateMixedMessage(components)
