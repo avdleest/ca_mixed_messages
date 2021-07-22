7977b0c6b04859ee00abfa31bf440648bdb7b485
Arthur van der Leest <avdleest@gmail.com>
2021-07-22 12:11:22 +0200

    Made the code slimmer ESLint enabled

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,23 +1,19 @@
-/* 
+/*
 Mixed messages produces a message with random content from three arrays.
 The message has the following structure:
-`Did you know that [A subject] [some achievement or possession] [timespan]?`*/
+`Did you know that [A subject] [some achievement or possession] [timespan]?`
+*/
 
-let subject = ['Trump', 'your mother', 'my guinea pig', 'your grandpa', 'your car', 'the Pope', 'his sister', 'the rabbit']
-let posessionOrAchievement =  ['has petted the most guinea pigs', 'has held the best speech', 'drifted the longest distance', 'won the most races']
-let timespan = ['in human history', 'of all times', 'today', 'presently']
+const subjectArr = ['Trump', 'your mother', 'my guinea pig', 'your grandpa', 'your car', 'the Pope', 'his sister', 'the rabbit']
+const posessionArr = ['has petted the most guinea pigs', 'has held the best speech', 'drifted the longest distance', 'won the most races']
+const timespanArr = ['in human history', 'of all times', 'today', 'presently']
 
-let components = [subject, posessionOrAchievement, timespan]
+const components = [subjectArr, posessionArr, timespanArr]
 
-const getRandomItem = (arr) => {
-    let index = Math.floor(Math.random() * arr.length);
-    return arr[index]
-}
+const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)]
 
 const generateMixedMessage = (arr) => {
-    let [subject, posession, timespan] = arr.map(el => getRandomItem(el))
-    
-    console.log(`Did you know that`, [subject, posession, timespan].join(" "));
+  console.log('Did you know that', arr.map(getRandomItem).join(' '))
 }
 
 generateMixedMessage(components)
